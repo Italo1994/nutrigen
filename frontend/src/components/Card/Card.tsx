ee4152800d85ff6bad4950aa2b74c1e5538bc8dd
Italo1994 <italohortiz2015@gmail.com>
2025-10-23 15:59:22 -0300

    feat: create card component

---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,52 @@
+import { ReactNode } from "react"
+
+interface CardProps {
+    children?: ReactNode;
+    classes?: string; 
+}
+
+export function Card({ children, classes }: CardProps) {
+    return (
+        <div className={`min-w-[15rem] ${classes} min-h-[15rem] p-4`}>
+            {children}
+        </div>
+    )
+}
+
+export function CardHeader({ children, classes }: CardProps) {
+    return (
+        <div className={`w-full mb-2 pb-2 ${classes}`}>
+            {children}
+        </div>
+    )
+}
+
+export function CardTitle({ children, classes }: CardProps) {
+    return (
+        <h2 className={`text-lg ${classes} font-semibold`}>{children}</h2>
+    )
+}
+
+export function CardDescription({ children, classes }: CardProps) {
+    return (
+        <p className={`text-sm ${classes}`}>
+            {children}
+        </p>
+    )
+}
+
+export function CardContent({ children }: CardProps) {
+    return (
+        <div className="py-2">
+            {children}
+        </div>
+    )
+}
+
+export function CardFooter({ children }: CardProps) {
+    return (
+        <div className="mt-2 pt-2">
+            {children}
+        </div>
+    )
+}
