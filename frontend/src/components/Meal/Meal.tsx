44af55f2c00ed87773f27fa00cd3cd5336072e78
Italo1994 <italohortiz2015@gmail.com>
2025-11-11 17:40:03 -0300

    feat: create meal component

---
+++ frontend/src/components/Meal/Meal.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { Button } from "../Button/Button"
+import { Card, CardDescription, CardHeader, CardTitle } from "../Card/Card"
+import { List } from "../List/List"
+import { searchFood } from "@/api/connect-fat-secret";
+
+interface FoodData {
+  food_name: string;
+  food_description: string;
+}
+
+export function Meal() {
+    const date = new Date()
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    const [foods, setFoods] = useState<string[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchFoods() {
+        try {
+            const data = await searchFood("arroz"); // 🔹 termo fixo só para teste
+            console.log("data ", data)
+            const results = data?.foods?.food?.map((item: any) => item.food_name) || [];
+            setFoods(results);
+        } catch (err) {
+            console.error("Erro ao carregar alimentos:", err);
+        } finally {
+            setLoading(false);
+        }
+        }
+
+        fetchFoods();
+    }, []);
+  
+
+    return (
+        <div className="shadow-[0_4px_15px_rgba(0,0,0,0.3)] p-2 w-full">
+            <div className="grid grid-cols-1 lg:grid-cols-3">
+                <div className="h-[10rem]">
+                    something
+                </div>
+                <div className="lg:col-span-2">
+                    <Card classes="w-full">
+                        <CardHeader classes="grid gap-7 pl-2 w-full">
+                            <span className="">{day}/{month}/{year}</span>
+                            <CardTitle classes="w-full">title</CardTitle>
+                            <CardDescription classes="flex gap-15 pr-4 w-full">
+                                <span>350</span>
+                                <span>350</span>
+                                <span>350</span>
+                            </CardDescription>
+                        </CardHeader>
+                    </Card>
+                </div>
+            </div>
+            <div>
+                <List
+                    title="ingredients"
+                    items={foods.length ? foods : ["No foods found"]}
+                />
+                <div className="flex justify-between pr-5 mt-7 md:mt-10 lg:mt-15">
+                    <Button label="Substitute..." classes="cursor-pointer font-bold" />
+                    <Button label="View Recipe >" classes="cursor-pointer font-bold text-blue-400" />
+                </div>
+            </div>
+        </div>
+    )
+}
